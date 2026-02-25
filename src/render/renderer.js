d5c0e273019239a2da3a14f928479b5682796f13
Vince G. <vg188705@gmail.com>
2026-02-25 14:46:40 -0500

    Update renderer.js

--- src/render/renderer.js
+++ src/render/renderer.js
@@ -1,10 +1,12 @@
 import { drawMoon } from './moon.js';
 import { drawComets } from './comet.js';
 import { drawBricks } from './castle.js';
+import { drawParticles } from './particles.js';
 
 export function render(state, ctx) {
   ctx.clearRect(0, 0, 480, 900);
   drawBricks(ctx, state.bricks);
   drawComets(ctx, state.comets);
   drawMoon(ctx, state.moon);
+  drawParticles(ctx, state.particles);
 }
