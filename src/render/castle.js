1e60a5d09cbd094034588214fadce8515121b77b
Vince G. <vg188705@gmail.com>
2026-02-25 14:44:21 -0500

    Update castle.js

--- src/render/castle.js
+++ src/render/castle.js
@@ -1,7 +1,12 @@
 export function drawBricks(ctx, bricks) {
   bricks.forEach(b => {
     if (!b.alive) return;
-    ctx.fillStyle = b.color;
+    const dmg = b.damage || 0;
+    ctx.fillStyle = `rgba(160,140,200,${1 - dmg})`;
     ctx.fillRect(b.x, b.y, b.w, b.h);
+    if (dmg > 0.5) {
+      ctx.strokeStyle = '#000';
+      ctx.strokeRect(b.x, b.y, b.w, b.h);
+    }
   });
 }
